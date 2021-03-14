b772b9189d3fe361d603623245003697cfb406d8
sureshmv8 <sureshmv8@gmail.com>
2021-03-13 22:21:47 -0500

    Weblogic version of bookstore with JNDI configured for data source lookup

---
+++ src/net/codejava/javaee/bookstore/BookDAO.java
@@ -0,0 +1,168 @@
+package net.codejava.javaee.bookstore;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class BookDAO {
+	private Connection jdbcConnection;
+	
+	public BookDAO() throws SQLException {
+		initDb();
+	}
+
+	private void initDb() throws SQLException {
+		String createSql = "CREATE TABLE `book` (\n" +
+				"  `book_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+				"  `title` varchar(128) NOT NULL,\n" +
+				"  `author` varchar(45) NOT NULL,\n" +
+				"  `price` float NOT NULL,\n" +
+				"  PRIMARY KEY (`book_id`),\n" +
+				"  UNIQUE KEY `book_id_UNIQUE` (`book_id`),\n" +
+				"  UNIQUE KEY `title_UNIQUE` (`title`)\n" +
+				") ";
+		connect();
+
+		Statement statement = jdbcConnection.createStatement();
+
+		statement.executeUpdate(createSql);
+		statement.close();
+		disconnect();
+
+		Book book1 = new Book("Thinking in Java", "Bruce Eckel", 25.69f);
+		insertBook(book1);
+		Book book2 = new Book("Effective Java", "Joshua Bloch", 27.88f);
+		insertBook(book2);
+
+	}
+
+	protected void connect() throws SQLException {
+		if (jdbcConnection == null || jdbcConnection.isClosed()) {
+
+			Hashtable ht = new Hashtable();
+			ht.put(Context.INITIAL_CONTEXT_FACTORY,
+					"weblogic.jndi.WLInitialContextFactory");
+			Context initContext = null;
+			try {
+				initContext = new InitialContext(ht);
+				DataSource ds = (DataSource)initContext.lookup("java:comp/myh2");
+				jdbcConnection = ds.getConnection();
+
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	
+	protected void disconnect() throws SQLException {
+		if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+			jdbcConnection.close();
+		}
+	}
+	
+	public boolean insertBook(Book book) throws SQLException {
+		String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setString(1, book.getTitle());
+		statement.setString(2, book.getAuthor());
+		statement.setFloat(3, book.getPrice());
+		
+		boolean rowInserted = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowInserted;
+	}
+	
+	public List<Book> listAllBooks() throws SQLException {
+		List<Book> listBook = new ArrayList<>();
+		
+		String sql = "SELECT * FROM book";
+
+		connect();
+
+		Statement statement = jdbcConnection.createStatement();
+		ResultSet resultSet = null;
+		resultSet = statement.executeQuery(sql);
+
+		while (resultSet.next()) {
+			int id = resultSet.getInt("book_id");
+			String title = resultSet.getString("title");
+			String author = resultSet.getString("author");
+			float price = resultSet.getFloat("price");
+			
+			Book book = new Book(id, title, author, price);
+			listBook.add(book);
+		}
+		
+		resultSet.close();
+		statement.close();
+		
+		disconnect();
+		
+		return listBook;
+	}
+	
+	public boolean deleteBook(Book book) throws SQLException {
+		String sql = "DELETE FROM book where book_id = ?";
+		
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setInt(1, book.getId());
+		
+		boolean rowDeleted = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowDeleted;		
+	}
+	
+	public boolean updateBook(Book book) throws SQLException {
+		String sql = "UPDATE book SET title = ?, author = ?, price = ?";
+		sql += " WHERE book_id = ?";
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setString(1, book.getTitle());
+		statement.setString(2, book.getAuthor());
+		statement.setFloat(3, book.getPrice());
+		statement.setInt(4, book.getId());
+		
+		boolean rowUpdated = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowUpdated;		
+	}
+	
+	public Book getBook(int id) throws SQLException {
+		Book book = null;
+		String sql = "SELECT * FROM book WHERE book_id = ?";
+		
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setInt(1, id);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		if (resultSet.next()) {
+			String title = resultSet.getString("title");
+			String author = resultSet.getString("author");
+			float price = resultSet.getFloat("price");
+			
+			book = new Book(id, title, author, price);
+		}
+		
+		resultSet.close();
+		statement.close();
+		
+		return book;
+	}
+}
